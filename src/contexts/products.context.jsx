5c0329af2fd988ebd6ae080e37d7766081ffca7f
Jin <jinkun1998@Jins-MacBook-Pro-14.local>
2023-03-30 20:18:04 +0700

    Init section 9

---
+++ src/contexts/products.context.jsx
@@ -0,0 +1,12 @@
+import { createContext, useState } from "react";
+
+import PRODUCTS from "../shop-data.json"
+
+export const ProductContext = createContext({
+    products: []
+})
+
+export const ProductProvider = ({ children }) => {
+    const [products, setProducts] = useState(PRODUCTS)
+    return <ProductContext.Provider value={children} />
+}
